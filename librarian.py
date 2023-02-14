b0d6dadf21c73ab2e6a5511289b2286c5730491e
vio <ultraviolette-22@yandex.ru>
2023-02-15 01:06:44 +0300

    modified class librarian

--- librarian.py
+++ librarian.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
@@ -6,42 +6,45 @@ import sqlite3
 from ebook import books, author_genre_book, authors, favorite, genres
 
 
+__doc__ = """
+This class is designed to be used with the e-library.db
+"""
+
+
 class Librarian:
     def __init__(self):
-        # create the main window
-        self.main_window = tkinter.Tk()
+        # creation of the main window for the application
+        self.main_window = tk.Tk()
         self.main_window.title("Library Management System")
         
         # layout main window content
         self.__build_main_window()
 
         # start the main loop
-        tkinter.mainloop()
+        tk.mainloop()
 
-    # build the main window
+    # building the main window
     def __build_main_window(self):
-        # create prompt label for user
+        # prompt label for user
         self.__create_prompt_label()
 
         # Listbox widget frame layout
         self.__build_listbox_frame()
 
-        # create quit button
+        # quit button
         self.__create_quit_button()
 
-    # create prompt label for user
+    # creation of prompt label for user
     def __create_prompt_label(self):
-        self.prompt_label = tkinter.Label(
-            self.main_window, text="Welcome to the Library Management System")
+        self.prompt_label = tk.Label(
+            self.main_window, text="Choose a book")
         self.prompt_label.grid(row=0, column=0, columnspan=2)
         self.prompt_label.config(font=("Helvetica", 12))
         self.prompt_label.pack(side='top', padx='5', pady='5')
 
     # Listbox and Scrollbar widgets frame layout
     def __build_listbox_frame(self):
-        self.listbox_frame = tkinter.Frame(self.main_window)
-        
-
+        self.listbox_frame = tk.Frame(self.main_window)
         self.__setup_listbox()  # setup listbox widget
 
         # create scrollbar for listbox widget elements
@@ -51,13 +54,13 @@ class Librarian:
         self.__fill_listbox()
 
         # pack listbox widget frame
-        self.listbox_frame.pack(side='top', padx='5', pady='5')
+        self.listbox_frame.pack()
 
     # create a listbox widget to display book titles on the screen
     def __setup_listbox(self):
         # creation of listbox widget
-        self.listbox = tkinter.Listbox(
-            self.listbox_frame, selectmode=tkinter.SINGLE, height=10)
+        self.listbox = tk.Listbox(
+            self.listbox_frame, selectmode=tk.SINGLE, height=6)
 
         # bind widget Listbox to a callback function
         self.listbox.bind('<<ListboxSelect>>', self.__get_details)
@@ -65,21 +68,22 @@ class Librarian:
         # pack listbox widget
         self.listbox.pack(side='left', padx='5', pady='5')
 
-    # create a vertical scrollbar widget for using with listbox widget
+    # creation of a vertical scrollbar widget for using with listbox widget
     def __create_scrollbar(self):
-        self.scrollbar = tkinter.Scrollbar(
-            self.listbox_frame, orient=tkinter.VERTICAL, command=self.listbox.yview)
+        self.scrollbar = tk.Scrollbar(
+            self.listbox_frame, orient=tk.VERTICAL)
+        self.scrollbar.config(command=self.listbox.yview)
         self.listbox.config(yscrollcommand=self.scrollbar.set)
-        self.scrollbar.pack(side="right", fill=tkinter.Y)
+        self.scrollbar.pack(side="right", fill=tk.Y)
 
     # display book titles on the listbox widget
     def __fill_listbox(self):
         for book in self.__get_books():
-            self.listbox.insert(tkinter.END, book.title)
+            self.listbox.insert(tk.END, book)
 
     # create quit button
     def __create_quit_button(self):
-        self.quit_button = tkinter.Button(
+        self.quit_button = tk.Button(
             self.main_window, text="Quit", command=self.main_window.destroy)
         self.quit_button.pack(side='top', padx='10', pady='5')
 
@@ -97,25 +101,29 @@ class Librarian:
             
     # get details of selected book
     def __get_details(self, event):
+        listbox_index = self.listbox.curselection()[0]
+        selected_book = self.listbox.get(listbox_index)
+            
+        # request in the books table from e-library.db details about selected_book
         try:
-            book_title = self.listbox.get(self.listbox.curselection()[0])
-            self.__get_book_details(book_title)
-            self.listbox.selection_clear(0, tkinter.END)    # ?????
-            self.__display_details_books()
+            get_details = author_genre_book.cursor.execute(
+                'SELECT * FROM author_genre_book WHERE book_id == ?',
+                (selected_book,))
+            results = get_details.fetchone()
+            
+            # display details about selected book
+            self.__display_details(results)
         
-        except IndexError:
-            pass
-        
-    # display details of selected book
-    def __get_book_details(self, book_title):
-        try:
-            book = books.get_book(book_title)
-            self.__display_book_details(book)
-            messagebox.showinfo("Information: ", book.title)
         except sqlite3.OperationalError:
             messagebox.showerror(
                 "Database Error", "Database connection error")
+            return
+        
+        finally:
+            if author_genre_book.connection != None:
+                author_genre_book.connection.close()
             
+
 
 # main function
 if __name__ == "__main__":
