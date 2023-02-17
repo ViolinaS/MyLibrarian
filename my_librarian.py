3f89eeb59c23948f629faf55e61d345aba925ae8
vio <ultraviolette-22@yandex.ru>
2023-02-17 21:47:03 +0300

    changed the entry for the filepath to the book

--- my_librarian.py
+++ my_librarian.py
@@ -1,7 +1,6 @@
 from ebook_sql_db import books, authors, genres, favorite
 import tkinter as tk
-import tkinter.messagebox
-from tkinter import ttk, filedialog, Event
+from tkinter import ttk, filedialog, Event, messagebox
 
 
 class MyLibrarian:
@@ -76,100 +75,108 @@ class MyLibrarian:
         # Book's Title entry with its label
         self.title_lable = tk.Label(self.new_book_frame, text="Book's title: ")
         self.title_entry = tk.Entry(self.new_book_frame)
+        self.title_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
+        self.title_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
         
         # Book's Author entry with its label
         self.author_lable = tk.Label(self.new_book_frame, text="Author's name: ")
         self.author_entry = tk.Entry(self.new_book_frame)
+        self.author_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
+        self.author_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
         
         # Book's Genre entry with its label
         self.genre_lable = tk.Label(self.new_book_frame, text="Genre: ")
         self.genre_entry = tk.Entry(self.new_book_frame)
+        self.genre_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
+        self.genre_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
         
         # Book's Release year entry with its label
         self.release_year_lable = tk.Label(self.new_book_frame, text="Release year: ")
         self.release_year_entry = tk.Entry(self.new_book_frame)
+        self.release_year_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
+        self.release_year_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
         
         # Book's Description entry for Text with its label
         self.description_lable = tk.Label(self.new_book_frame, text="Description: ")
         self.description_entry = tk.Text(self.new_book_frame, width=500, height=5)
+        self.description_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
+        self.description_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
         
         # Book's Publisher entry with its label
         self.publisher_lable = tk.Label(self.new_book_frame, text="Publisher: ")
         self.publisher_entry = tk.Entry(self.new_book_frame)
+        self.publisher_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
+        self.publisher_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
         
         # Book's filepath on the file system
         self.filepath_lable = tk.Label(self.new_book_frame, text="Filepath to your book: ")
-        
-        # Building the menu entries
-        self.title_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.title_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.author_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.author_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.genre_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.genre_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.release_year_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.release_year_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.description_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.description_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.publisher_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-        self.publisher_entry.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
         self.filepath_lable.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
+        self.filepath_entry = filedialog.askopenfilename()
+        self.filepath_entry_text = tk.Entry(self.new_book_frame)
+        self.filepath_entry_text.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
+        self.filepath_entry_text.insert(tk.END, self.filepath_entry)
         
-        
-        # create the browse button
-        self.filepath_button = tk.Button(self.new_book_frame, text="Browse", command= self.browse_file)
-        self.filepath_button.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
         
         # create ok button
-        self.ok_button = tk.Button(self.new_book_frame, text="OK", command=self.add_book_to_db)
+        self.ok_button = tk.Button(self.new_book_frame, text="OK", command=self.save_book_to_db)
         self.ok_button.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
         
         # create cancel button
         self.cancel_button = tk.Button(self.new_book_frame, text="Cancel", command=self.new_book_window.destroy)
         self.cancel_button.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
         
-    def browse_file(self):
-        self.filepath_entry = filedialog.askdirectory()
-        return self.filepath_entry
+
     
-    def add_book_to_db(self):
+        
+    
+    # def add_book_to_db(self):
+        # __doc__ = """
+        # This function is used to write a new book to the e-library.db
+        # """
+        
+        # try:
+            # filepath = self.filepath_entry
+            # print(filepath)
+            # books.cursor.execute("INSERT INTO books (title, author_id, genre_id, release_year, description,\
+            # publisher, link) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                        # (self.title_entry.get(), self.author_entry.get(), self.genre_entry.get(), self.release_year_entry.get(),
+                        # self.description_entry.get("1.0", tk.END), self.publisher_entry.get(), filepath))
+            # books.conn.commit()
+            
+        # except:
+            # messagebox.showerror("Error", "Something went wrong")
+            
+    def save_book_to_db(self):
         __doc__ = """
-        This function is used to write a new book to the e-library.db
+        This function is used to confirm a new book to the e-library.db
         """
-        filepath = self.browse_file
-        try:
-            if filepath == True:
-                books.cursor.execute("INSERT INTO books (title, author, genre, release_year, description,\
-                publisher, link) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                        (self.title_entry.get(), self.author_entry.get(), self.genre_entry.get(), self.release_year_entry.get(),
-                        self.description_entry.get("1.0", tk.END), self.publisher_entry.get(), filepath))
-                tk.messagebox.showinfo("added book")
-        except:
-            tk.messagebox.showerror("Error", "Something went wrong")
+        
+        self.new_book_window.destroy()
+        messagebox.showinfo("Saved book", "Book saved successfully")
 
     # create self.delete_book function
     def delete_book(self):
-        tk.messagebox.showinfo("Delete a Book", "Delete the title of the book")
+        messagebox.showinfo("Delete a Book", "Delete the title of the book")
     
     # create self.find_book function
     def find_book(self):
-        tk.messagebox.showinfo("Find a Book", "Find the title of the book")
+        messagebox.showinfo("Find a Book", "Find the title of the book")
         
     # create self.book_shelf function
     def book_shelf(self):
-        tk.messagebox.showinfo("Book's Shelf", "Book's shelf")
+        messagebox.showinfo("Book's Shelf", "Book's shelf")
         
     # create self.favorites function
     def favorites(self):
-        tk.messagebox.showinfo("Favorites", "Favorites")
+        messagebox.showinfo("Favorites", "Favorites")
     
     # create self.help function
     def help(self):
-        tk.messagebox.showinfo("Help", "Help")
+        messagebox.showinfo("Help", "Help")
     
     # create self.exit function
     def exit(self):
-        tk.messagebox.showinfo("Exit", "Exit")
+        messagebox.showinfo("Exit", "Exit")
 
 
 
